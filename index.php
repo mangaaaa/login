c477ba65e6eb908fbcb9eb23f6e0bec9cb8f83a8
Ba Mang <76155923+mangaaaa@users.noreply.github.com>
2021-03-31 13:14:20 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<?
+$host = "ec2-23-22-191-232.compute-1.amazonaws.com";
+$port = "5432";
+$dbname = "d9kre3i7166g8k";
+$user = "gmpgirnharzwij";
+$password = "9747f761fd35e62bfb0e8db8b073598e6f674ee9ae63563a4b6c00ea3e9e11e0"; 
+$connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
+$dbconn = pg_connect($connection_string);
+if(isset($_POST['submit'])&&!empty($_POST['submit'])){
+    
+    $hashpassword = md5($_POST['pwd']);
+    $sql ="select *from public.login where us = '".pg_escape_string($_POST['email'])."' and pw ='".$hashpassword."'";
+    $data = pg_query($dbconn,$sql); 
+    $login_check = pg_num_rows($data);
+    if($login_check > 0){ 
+        
+        echo "Login Successfully";    
+    }else{
+        
+        echo "Invalid Details";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>PHP PostgreSQL Registration & Login Example </title>
+  <meta name="keywords" content="PHP,PostgreSQL,Insert,Login">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <h2>Login Here </h2>
+  <form method="post">
+  
+     
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+    
+     
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+    </div>
+     
+    <input type="submit" name="submit" class="btn btn-primary" value="Submit">
+  </form>
+</div>
+</body>
+</html>
