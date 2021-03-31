1180dadee898e5841cd09cf65746dfa993b28d4d
Ba Mang <76155923+mangaaaa@users.noreply.github.com>
2021-03-31 14:20:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if($dbconn){
 }
 if(isset($_POST['submit'])&&!empty($_POST['submit'])){
     $hashpassword = md5($_POST['pwd']);
-    $sql ="select *from public.login where email = '".pg_escape_string($_POST['email'])."' and pw ='".$hashpassword."'";
+    $sql ="SELECT *FROM public.login WHERE email = '".pg_escape_string($_POST['email'])."' and pw ='".$hashpassword."'";
     $data = pg_query($dbconn,$sql); 
     $login_check = pg_num_rows($data);
     if($login_check > 0){ 
@@ -35,7 +35,7 @@ if(isset($_POST['submit'])&&!empty($_POST['submit'])){
 <body>
 <div class="container">
   <h2>Login Here </h2>
-  <form method="post">
+  <form method="POST">
   
      
     <div class="form-group">
